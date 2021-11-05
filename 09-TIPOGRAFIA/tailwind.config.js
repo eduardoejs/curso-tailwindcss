a329a73e463e7ec8c700ee137069d3e655b8dd36
Eduardo Silva <eduardo.ejs@hotmail.com>
2021-11-05 10:18:36 -0300

    Finalizado modulo 9 sobre tipografia

---
+++ 09-TIPOGRAFIA/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'arial': 'Arial',
+        'roboto': 'Roboto',
+        'Rampart': ['Rampart One', "cursive"],
+        'Mulish': ['Mulish', 'sans-serif'],
+        'Fruktur': ['Fruktur', 'sans-serif'],
+        'Poppins': ['Poppins', 'sans-serif'],
+      },
+      fontSize: {
+        '12xl': '8rem',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
