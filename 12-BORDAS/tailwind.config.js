f3e384802356d40760adc5173e2c1bbc3f666cb7
Eduardo Silva <eduardo.ejs@hotmail.com>
2021-11-08 11:06:38 -0300

    Finalizado o modulo 12 sobre bordas

---
+++ 12-BORDAS/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'arial': 'Arial',
+        'roboto': ['Roboto', 'sans-serif'],
+        'abril': ['Abril Fatface', 'cursive'],
+        'Rampart': ['Rampart One', "cursive"],
+        'Mulish': ['Mulish', 'sans-serif'],
+        'Fruktur': ['Fruktur', 'sans-serif'],
+        'Poppins': ['Poppins', 'sans-serif'],
+        'ubuntu': ['Ubuntu', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
