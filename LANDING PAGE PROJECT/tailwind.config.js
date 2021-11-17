2578cdfaa7f3b6d6bf162bfdd38372216f467fa9
Eduardo Silva <eduardo.ejs@hotmail.com>
2021-11-17 08:39:24 -0300

    definindo exemplos

--- LANDING PAGE PROJECT/tailwind.config.js
+++ LANDING PAGE PROJECT/tailwind.config.js
@@ -8,8 +8,11 @@ module.exports = {
       colors: {
         "appcolor-purple": "#5267DF",        
         "appcolor-blue": "#242A45",
-        "appcolor-gray": "#9194A2",
+        "appcolor-red": "#901717",
+        "appcolor-gray": "#495661",
+        "appcolor-gray-100": "#9194A2",
         "appcolor-white": "#F7F7F7",
+        "appcolor-gold": "#ffd739",
         "coolGray": colors.coolGray,
       },
     },
