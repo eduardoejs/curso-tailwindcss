2c22eafccba897f609e0b8d54b96c05a50159d18
Eduardo Silva <eduardo.ejs@hotmail.com>
2021-11-24 08:44:13 -0300

    perdido

--- LANDING PAGE PROJECT/tailwind.config.js
+++ LANDING PAGE PROJECT/tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
     extend: {
       colors: {
         "appcolor-purple": "#5267DF",        
+        "appcolor-purple-2": "#a200f9", 
         "appcolor-blue": "#242A45",
+        "appcolor-blue-light": "#004eff",
         "appcolor-red": "#901717",
         "appcolor-gray": "#495661",
         "appcolor-gray-100": "#9194A2",
@@ -15,16 +17,27 @@ module.exports = {
         "appcolor-gold": "#ffd739",
         "coolGray": colors.coolGray,
       },
+      backgroundImage: theme => ({
+        'herosection': "url('../img/vectors/undraw_winners_ao-2-o.svg')",
+        'herosection2': "url('../img/17545.jpg')",
+        'abstract': "url('../img/bgs/5026563.jpg')",
+        'globe': "url('../img/bgs/Vector_2646.jpg')",
+        'digital-map': "url('../img/bgs/digital-world-map-hologram-blue-background.jpg')",        
+      }),
     },
     fontFamily: {
-      Ubuntu: ["Ubuntu", "sans-serif"],
+      Nunito: ["Nunito", "sans-serif"],
       Poppins: ["Poppins", "sans-serif"],
       Roboto: ["Roboto", "sans-serif"],
       Inter: ["Inter", "sans-serif"],
+      Lobster: ["Lobster", "cursive"],
+      Montserrat: ["Montserrat", "sans-serif"],
+      Kaushan: ["Kaushan Script", "cursive"],
+      Carter: ["Carter One", "cursive"],
     },
     container: {
       center: true,
-      padding: "1rem",
+      padding: '1rem',
     }
   },
   variants: {
